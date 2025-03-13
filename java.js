ab45dbc9db0b620b940a043a01c24e75a89adcdf
Timothy Williams <113715036+shwifty-man@users.noreply.github.com>
2025-03-13 12:40:31 -0400

    fix spelling and styles

--- java.js
+++ java.js
@@ -4,33 +4,33 @@ const text = document.querySelector(".second-name");
 
 //Sticks the nav bar to the screen when scrolling:
 function stickNav() {
-    if(window.scrollY > 0) {
+  if (window.scrollY > 0) {
     nav.style.position = "sticky";
-    nav.style.top = '0';
-    nav.style.width = '100%'
-    nav.style.backgroundColor = "white"
-    nav.style.zIndex = '1000'
-    } else {
-        nav.style.position = 'static'
-    }
+    nav.style.top = "0";
+    nav.style.width = "100%";
+    nav.style.backgroundColor = "white";
+    nav.style.zIndex = "1000";
+  } else {
+    nav.style.position = "static";
+  }
 }
 
 //Makes the h2 visible after scrolling past a certian point:
 function textAppear() {
-    if(window.scrollY > 500){
-        text.style.visibility = "visible";
-    } else {
-        text.style.visibility = "hidden";
-    }
+  if (window.scrollY > 500) {
+    text.style.visibility = "visible";
+  } else {
+    text.style.visibility = "hidden";
+  }
 }
 
 //Makes the small pfp appear after scrolling past a certain point:
 function imgAppear() {
-    if (window.scrollY > 500) {
-      pfpImg.style.visibility = "visible";
-    } else {
-      pfpImg.style.visibility = "hidden";
-    }
+  if (window.scrollY > 500) {
+    pfpImg.style.visibility = "visible";
+  } else {
+    pfpImg.style.visibility = "hidden";
+  }
 }
 // Add changing background from image to different image or color:
 document.addEventListener("DOMContentLoaded", function () {
@@ -46,13 +46,15 @@ document.addEventListener("DOMContentLoaded", function () {
       setTimeout(() => {
         introContainer.style.display = "none"; // Completely hide the intro after fade-out
         content.classList.add("show-content"); // Show the main content
-      }, 500); // Wait for the fade-out effect to complete (500ms)
-    }, 2500); // Wait for 2.5 seconds before triggering the fade-out
+      }, 300); // Wait for the fade-out effect to complete (500ms)
+    }, 1500);
   });
 });
 
-document.getElementById("composeEmail").addEventListener("click", function (event) 
-{   event.preventDefault(); // Prevent the default link behavior
+document
+  .getElementById("composeEmail")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior
 
     let message = document.getElementById("emailBody").value;
     let encodedMessage = encodeURIComponent(message); // Encode text for URL
